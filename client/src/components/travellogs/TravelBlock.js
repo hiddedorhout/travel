4f388bb32a89c1f2951c41ac24a339038bc2296a
hdorhout <devdorhout@gmail.com>
2019-01-24 21:57:02 +0100

    block state and duration

--- client/src/components/travellogs/TravelBlock.js
+++ client/src/components/travellogs/TravelBlock.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
 import './travelblock.css'
+import moment from "moment";
 
 
 class TravelBlock extends Component {
 
-	state = {active: false, startTime: null, endTime: null}
+	state = {active: false, startTime: null, endTime: null, duration: null}
 
 	setActive = () => {
 		if (!this.state.active) {
-			this.setState({startTime: Date.now()})
+			this.setState(prevState => ({ startTime: moment().format("DD-MM-YYYY HH:mm:ss"), active: !prevState.active}))
 		}
 		if (this.state.active && this.state.startTime !== null) {
-			this.setState({endTime: Date.now()})
+			const end = moment().format("DD-MM-YYYY HH:mm:ss")
+			const duration = moment.duration(moment(end, "DD-MM-YYYY HH:mm:ss") - moment(this.state.startTime, "DD-MM-YYYY HH:mm:ss"));
+			this.setState({endTime: end, duration})
 		}
 
-		this.setState(prevState => ({active: !prevState.active}))
+		if (this.state.duration !== null) {
+			this.setState(prevState => ({ active: !prevState.active, startTime: null, endTime: null, duration: null}))
+		}
+
 	}
 
 	render(){
 		return(
-			<div className="block" onClick={this.setActive}>
+			<div className="block" id={this.state.active.toString()} onClick={this.setActive}>
 				<div className="blockHeader">
 					<div className="image" />
 					{this.props.transport}
 				</div>
-				{this.state.startTime}
-				{this.state.endTime}
+				<div className="timeBlock">
+					<div className="time">
+						Start: {this.state.startTime}
+					</div>
+					<div className="time">
+						End: {this.state.endTime}
+					</div>
+				</div>
 			</div>
 		)
 	}
