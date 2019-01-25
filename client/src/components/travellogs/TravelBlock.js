95fc71afe0c3b61de45cf5aa65c6bca058ab3628
hdorhout <devdorhout@gmail.com>
2019-01-25 11:25:45 +0100

    setTime

--- client/src/components/travellogs/TravelBlock.js
+++ client/src/components/travellogs/TravelBlock.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './travelblock.css'
 import moment from "moment";
+import { timeout } from 'q';
 
 
 class TravelBlock extends Component {
@@ -9,34 +10,105 @@ class TravelBlock extends Component {
 
 	setActive = () => {
 		if (!this.state.active) {
-			this.setState(prevState => ({ startTime: moment().format("DD-MM-YYYY HH:mm:ss"), active: !prevState.active}))
+			this.setState(prevState => ({ startTime: Date.now(), active: !prevState.active}))
 		}
 		if (this.state.active && this.state.startTime !== null) {
-			const end = moment().format("DD-MM-YYYY HH:mm:ss")
-			const duration = moment.duration(moment(end, "DD-MM-YYYY HH:mm:ss") - moment(this.state.startTime, "DD-MM-YYYY HH:mm:ss"));
-			this.setState({endTime: end, duration})
+			const endTime = Date.now()
+			const duration = endTime - this.state.startTime;
+			this.setState({endTime, duration})
 		}
 
 		if (this.state.duration !== null) {
 			this.setState(prevState => ({ active: !prevState.active, startTime: null, endTime: null, duration: null}))
 		}
+	}
 
+	addMinutes = (e, stateName) => {
+		e.preventDefault();
+		const newTime = this.state[stateName] + (60*1000)
+		this.setState({[stateName]: newTime});
+		if (stateName === "startTime") {
+			const newDuration = this.state.endTime - newTime
+			if (newDuration > 0) {
+				this.setState({duration: newDuration})
+			}
+		}
+		if (stateName === "endTime"){
+			const newDuration = newTime - this.state.startTime
+			if (newDuration > 0) {
+				this.setState({duration: newDuration})
+			}
+		}
+	}
+
+	minMinutes = (e, stateName) => {
+		e.preventDefault();
+		const newTime = this.state[stateName] - (60 * 1000)
+		this.setState({[stateName]: newTime});
+		if (stateName === "startTime") {
+			const newDuration = this.state.endTime - newTime
+			if (newDuration > 0) {
+				this.setState({duration: newDuration})
+			}
+		}
+		if (stateName === "endTime"){
+			const newDuration = newTime - this.state.startTime
+			if (newDuration > 0) {
+				this.setState({duration: newDuration})
+			}
+		}
 	}
 
 	render(){
 		return(
-			<div className="block" id={this.state.active.toString()} onClick={this.setActive}>
-				<div className="blockHeader">
+			<div className="main">
+				<div className="block">
+					<div className="blockHeader">
 					<div className="image" />
 					{this.props.transport}
+					</div>
+					<div className="timeBlock">
+					{
+						this.state.startTime !== null &&
+						<div className="timeDisplay">
+							<button className="manipulateTimeButton" onClick={e => this.minMinutes(e, "startTime")}>-1</button>
+							<div className="time">
+							Start: {moment(this.state.startTime).format("HH:mm")}
+							</div>
+							<button className="manipulateTimeButton" onClick={e => this.addMinutes(e, "startTime")}>+1</button>
+						</div>
+					}
+					{
+						this.state.endTime !== null &&
+						<div className="timeDisplay">
+							<button className="manipulateTimeButton" onClick={e => this.minMinutes(e, "endTime")}>-1</button>
+							<div className="time">
+							End: {moment(this.state.endTime).format("HH:mm")}
+							</div>
+							<button className="manipulateTimeButton" onClick={e => this.addMinutes(e, "endTime")}>+1</button>
+						</div>
+					}
+					{
+						this.state.duration !== null &&
+						<div className="time">
+						Duration: {moment.duration(this.state.duration).minutes()} minutes
+						</div>
+					}
+					</div>
 				</div>
-				<div className="timeBlock">
-					<div className="time">
-						Start: {this.state.startTime}
-					</div>
-					<div className="time">
-						End: {this.state.endTime}
-					</div>
+				<div className="starterStopper" id={this.state.active.toString()} onClick={this.setActive}>
+					{
+						!this.state.active && 
+							<span className="time">Start!</span>
+					}
+					{
+						this.state.active && this.state.endTime === null &&
+						<span className="time">Running</span>
+					}
+					{
+						this.state.endTime !== null &&
+							<span className="time">Stopped</span>
+					}
 				</div>
 			</div>
 		)
--- client/src/components/travellogs/Travellogs.js
+++ client/src/components/travellogs/Travellogs.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react'
 import './travellogs.css'
 import Block from "./TravelBlock.js"
+import moment from 'moment';
 
 class TravelLogs extends Component {
+
+	state = {sessionID:"" , active: false}
+
 	render(){
 		return(
 			<div className="travelLogPage">
-			<ul className="modeOfTransportList">
-			{
-				["Train", "Bus", "Bike"].map( (modeOfTransport, i) => {
-					return(
-						<li key={i} className="transportType">
-							<Block transport={modeOfTransport} index={i}/>
-						</li>		
-					)
-				})
-			}
-			</ul>
+				<div className="date">
+					{moment().format("dddd Do of MMMM YYYY",)}
+				</div>
+				<ul className="modeOfTransportList">
+				{
+					["Train", "Bus", "Bike"].map( (modeOfTransport, i) => {
+						return(
+							<li key={i} className="transportType">
+								<Block transport={modeOfTransport} index={i}/>
+							</li>		
+						)
+					})
+				}
+				</ul>
 			</div>
 		)
 	}
