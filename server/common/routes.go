d77e2fcea77d6c1d4fc401365bacbe02ca6789f5
hdorhout <devdorhout@gmail.com>
2019-01-30 08:13:27 +0100

    api docs and first setup routes

--- server/common/routes.go
+++ server/common/routes.go
@@ -10,40 +10,176 @@ import (
 func (s *System) SetupRoutes() {
 	http.HandleFunc("/travel/v1/login", s.login)
 	http.HandleFunc("/travel/v1/register", s.register)
+	http.HandleFunc("/travel/v1/travel-session", s.validateAuthorization(s.travelHandler))
+	http.HandleFunc("/travel/v1/routes-list", s.validateAuthorization(s.getRoutesList))
+	http.HandleFunc("/travel/v1/route", s.validateAuthorization(s.getRoute))
 }
 
 func (s *System) login(w http.ResponseWriter, r *http.Request) {
+	type loginRequestBody struct {
+		UserName string `json:"username"`
+		Password string `json:"password"`
+	}
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	type loginRequestBody struct {
-		UserName string `json:"username"`
-		Password string `json:"password"`
-	}
 	var rb loginRequestBody
 	if err := json.Unmarshal(body, &rb); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		errorResponseWriter(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	if len(rb.UserName) == 0 || len(rb.Password) == 0 {
-		http.Error(w, "Missing request parameters", http.StatusBadRequest)
+		errorResponseWriter(w, "Missing request parameters", http.StatusBadRequest)
 		return
 	}
 
-	if _, err := s.users.CheckPassword(rb.UserName, rb.Password); err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
+	id, err := s.users.CheckPassword(rb.UserName, rb.Password)
+	if err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 
 	// Create session cookie
+	session, err := s.sessions.GenerateSession(*id)
+	if err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(*session))
 
 }
 
 func (s *System) register(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		UserName string `json:"username"`
+		Password string `json:"password"`
+	}
 
+	type responseBody struct {
+		UserID string `json:"userID"`
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var rb requestBody
+	if err := json.Unmarshal(body, &rb); err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if len(rb.UserName) == 0 || len(rb.Password) == 0 {
+		errorResponseWriter(w, "Missing request body parameters", http.StatusBadRequest)
+		return
+	}
+	id, err := s.users.RegisterUser(rb.UserName, rb.Password)
+	if err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	respb := responseBody{
+		UserID: *id,
+	}
+
+	resp, err := json.Marshal(respb)
+	if err != nil {
+		errorResponseWriter(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(201)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func (s *System) travelHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "POST":
+		s.startTravelSession(w, r)
+	case "PUT":
+		s.updateTravelSession(w, r)
+	case "GET":
+		s.getTravelSession(w, r)
+	case "DELETE":
+		s.deleteTravelSession(w, r)
+	default:
+		errorResponseWriter(w, "Unsuported method", http.StatusNotImplemented)
+		return
+	}
+}
+
+func (s *System) startTravelSession(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) updateTravelSession(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) getTravelSession(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) deleteTravelSession(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) getRoutesList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) routesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		s.getRoute(w, r)
+	case "POST":
+		s.createRoute(w, r)
+	default:
+		errorResponseWriter(w, "Unsuported method", http.StatusNotImplemented)
+		return
+	}
+}
+
+func (s *System) getRoute(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *System) createRoute(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UTILS
+func errorResponseWriter(w http.ResponseWriter, message string, responseCode int) {
+	type errorResponse struct {
+		Reason string `json:"reason"`
+	}
+	rb := errorResponse{
+		Reason: message,
+	}
+	resp, _ := json.Marshal(rb)
+	w.WriteHeader(responseCode)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func (s *System) validateAuthorization(h func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if len(auth) == 0 {
+			errorResponseWriter(w, "Missing Authorization header", http.StatusBadRequest)
+		}
+		if err := s.sessions.ValidateSession(auth); err != nil {
+			errorResponseWriter(w, err.Error(), http.StatusUnauthorized)
+		}
+		h(w, r)
+	}
 }
--- server/services/users/users.go
+++ server/services/users/users.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/base64"
 
+	"github.com/google/uuid"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -16,19 +18,19 @@ type Service struct {
 func New(db *sql.DB) (*Service, error) {
 
 	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS users(
-		ID NOT NULL INTEGER PRIMARY KEY,
-		username TEXT NOT NULL,
+		ID NOT NULL TET PRIMARY KEY,
+		username TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL
 	)`); err != nil {
 		return nil, err
 	}
 
-	createUserStmnt, err := db.Prepare(`INSERT INTO users (username, password) VALUES (?,?)`)
+	createUserStmnt, err := db.Prepare(`INSERT INTO users (ID, username, password) VALUES (?,?,?)`)
 	if err != nil {
 		return nil, err
 	}
 
-	getUserStmnt, err := db.Prepare(`SELECT password FROM users WHERE username = ?`)
+	getUserStmnt, err := db.Prepare(`SELECT ID, password FROM users WHERE username = ?`)
 	if err != nil {
 		return nil, err
 	}
@@ -41,36 +43,39 @@ func New(db *sql.DB) (*Service, error) {
 }
 
 // RegisterUser creates a digest over
-func (s *Service) RegisterUser(username, plainTextPassword string) error {
+func (s *Service) RegisterUser(username, plainTextPassword string) (*string, error) {
+
+	id := uuid.New().String()
 
 	encryotedPwd, err := bcrypt.GenerateFromPassword([]byte(plainTextPassword), 10)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	b64Pwd := base64.StdEncoding.EncodeToString(encryotedPwd)
 
-	if _, err := s.createUserStmnt.Exec(username, b64Pwd); err != nil {
-		return err
+	if _, err := s.createUserStmnt.Exec(id, username, b64Pwd); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &id, nil
 }
 
 // CheckPassword authenticates the user and return true if user/password is ok
-func (s *Service) CheckPassword(username, password string) (bool, error) {
+func (s *Service) CheckPassword(username, password string) (*string, error) {
 	var b64EncodedPwd string
-	if err := s.getUserStmnt.QueryRow(username).Scan(&b64EncodedPwd); err != nil {
-		return false, err
+	var ID string
+	if err := s.getUserStmnt.QueryRow(username).Scan(&ID, &b64EncodedPwd); err != nil {
+		return nil, err
 	}
 
 	rawPwdHash, err := base64.StdEncoding.DecodeString(b64EncodedPwd)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(password), rawPwdHash); err != nil {
-		return false, err
+		return nil, err
 	}
 
-	return true, nil
+	return &ID, nil
 }
